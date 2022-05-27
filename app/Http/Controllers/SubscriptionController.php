5c74766e8cbdcaa5fbacc0704ac1f41196f54ee0
shivamkumar <shivam96342@gmail.com>
2022-05-28 03:53:47 +0530

    First Commit

---
+++ app/Http/Controllers/SubscriptionController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserData;
+use App\Models\UserDataSecondDb;
+use Validator;
+use DB;
+use Illuminate\Support\Facades\Schema;
+
+class SubscriptionController extends Controller
+{
+  
+    public function __construct()
+    {
+    
+    }
+
+    //-------------------------------------------------------------------
+
+   
+    public function store(Request $request)
+    {
+
+
+       //var_dump('hii');die();
+
+        print_r('hii');die();
+
+    }
+
+}
+//end of class
+//end of file
+
+
+
+
+
